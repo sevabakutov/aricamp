0fb49cd8d236dd10b72bf21a37c7e6e4981b00d1
Arik <kruftik203@gmail.com>
2026-09-08 09:56:43 +0200

    made task 1.1

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,68 @@
+#[derive(Debug, Default, Clone, Copy)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Polyline {
+    array: Vec<Point>,
+}
+
+impl Polyline {
+    fn add(&mut self, item: Point) {
+        self.array.push(item);
+    }
+
+    fn pop(&mut self) -> Point {
+        self.array.pop().unwrap_or(Point::default())
+    }
+
+    fn get(&self, index: usize) -> Point {
+        if self.array.len() > index {
+            return self.array[index]
+        }
+        Point::default()
+    }
+
+    fn remove(&mut self, index: usize) -> Point {
+        if self.array.len() > index {
+            return self.array.remove(index)
+        }
+        return Point::default()
+    }
+
+}
+
 fn main() {
-    println!("Implement me!");
+    let a = Point::default();
+    let mut b = a;
+    b.x = 15;
+    b.y = 15;
+
+    let c = Point {x: 30, y: 30};
+
+    let mut arr = Polyline { array: Vec::new() };
+
+    arr.add(a);
+    arr.add(b);
+
+    println!("{a:?}, {b:?}, {c:?}, {arr:?}");
+
+    let b = arr.pop();
+
+    arr.add(c);
+
+    println!("b: {b:?}, arr: {arr:?}");
+
+
+    let a = arr.get(0);
+    let d = arr.get(3);
+
+    println!("a: {a:?}, d: {d:?}");
+    println!("arr: {arr:?}");
+
+
+    let f = arr.remove(1);
+    println!("f: {f:?}, arr: {arr:?}");
 }
