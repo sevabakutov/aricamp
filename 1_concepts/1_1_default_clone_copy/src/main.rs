3237633280efa64e6d5790aed1d6f43c86fd5ce8
Arik <kruftik203@gmail.com>
2026-09-09 12:54:26 +0200

    task 1.3 + 1.1 done

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,28 +1,31 @@
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct Polyline {
     array: Vec<Point>,
 }
 
 impl Polyline {
-    fn add(&mut self, item: Point) {
+    fn push(&mut self, item: Point) {
         self.array.push(item);
     }
 
-    fn pop(&mut self) -> Point {
-        self.array.pop().unwrap_or(Point::default())
+    fn pop(&mut self) -> Option<Point> {
+        if self.array.len() > 1 {
+            return Some(self.array.pop().unwrap())
+        }
+        None
     }
 
-    fn get(&self, index: usize) -> Point {
+    fn get(&self, index: usize) -> Option<&Point> {
         if self.array.len() > index {
-            return self.array[index]
+            return Some(&self.array[index])
         }
-        Point::default()
+        None
     }
 
     fn remove(&mut self, index: usize) -> Point {
@@ -32,37 +35,72 @@ impl Polyline {
         return Point::default()
     }
 
+    fn new(array: Option<Vec<Point>>) -> Self {
+        Polyline {
+            array: match array {
+                Some(arr) => {
+                    if arr.len() >= 1{
+                        arr
+                    } else {
+                        vec![Point::default()]
+                    }
+                },
+                None => vec![Point::default()]
+            }
+        }
+    }
+
+}
+
+impl Default for Polyline {
+    fn default() -> Self {
+        Self { array: vec![Point::default()] }
+    }
 }
 
 fn main() {
-    let a = Point::default();
-    let mut b = a;
-    b.x = 15;
-    b.y = 15;
-
-    let c = Point {x: 30, y: 30};
-
-    let mut arr = Polyline { array: Vec::new() };
-
-    arr.add(a);
-    arr.add(b);
-
-    println!("{a:?}, {b:?}, {c:?}, {arr:?}");
-
-    let b = arr.pop();
-
-    arr.add(c);
-
-    println!("b: {b:?}, arr: {arr:?}");
+}
 
 
-    let a = arr.get(0);
-    let d = arr.get(3);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("a: {a:?}, d: {d:?}");
-    println!("arr: {arr:?}");
+    #[test]
+    fn test_point_default() {
+        let point: Point = Default::default();
+        assert_eq!(point, Point { x: 0, y: 0 });
+    }
 
+    #[test]
+    fn test_polyline_push_and_get() {
+        let mut polyline = Polyline::new(None);
+        let point = Point { x: 5, y: -3 };
+        polyline.push(point);
 
-    let f = arr.remove(1);
-    println!("f: {f:?}, arr: {arr:?}");
+        assert_eq!(polyline.get(0), Some(&Point::default()));
+        assert_eq!(polyline.get(1), Some(&point));
+        assert_eq!(polyline.get(2), None);
+    }
+
+    #[test]
+    fn test_polyline_pop() {
+        let mut polyline = Polyline::new(None);
+
+        assert_eq!(polyline.pop(), None);
+        assert_eq!(polyline.pop(), None);
+        assert_eq!(polyline.pop(), None);
+    }
+
+    #[test]
+    fn test_polyline_clone() {
+        let mut polyline = Polyline::new(None);
+        polyline.push(Point { x: 1, y: 1 });
+        let polyline_clone = polyline.clone();
+
+        assert_eq!(polyline, polyline_clone);
+
+        polyline.push(Point { x: 2, y: 2 });
+        assert_ne!(polyline, polyline_clone);
+    }
 }
--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,27 +1,41 @@
 use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 
-#[derive(Debug)]
-struct GlobalStack<T: Clone> {
+#[derive(Debug, PartialEq)]
+struct GlobalStack<T> {
     stack: Rc<RefCell<Vec<T>>>,
 }
 
-impl<T: Clone> GlobalStack<T> {
-    fn pop(&self) -> T{
-        self.stack.borrow_mut().pop().unwrap()
+impl<T> GlobalStack<T> {
+    fn pop(&self) -> Option<T>{
+        let mut stack = self.stack.borrow_mut();
+
+        if stack.len() > 0 {
+            Some(stack.pop().unwrap())
+        } else {
+            None
+        }
     }
 
     fn push(&self, item: T) {
         self.stack.borrow_mut().push(item);
     }
-
-    fn get_last(&self) -> T {
-        let a = self.stack.borrow();
-        a[a.len() - 1].clone()
+    fn new(stack: Option<Vec<T>>) -> GlobalStack<T>{
+        match stack {
+            None => GlobalStack { stack: Rc::new(RefCell::new(Vec::new())) },
+            Some(st) => GlobalStack { stack: Rc::new(RefCell::new(st)) }
+        }
     }
+}
 
-    fn new(stack: Vec<T>) -> GlobalStack<T>{
-        GlobalStack { stack: Rc::new(RefCell::new(stack)) }
+impl<T: Clone> GlobalStack<T> {
+    fn get_last(&self) -> Option<T> {
+        let a = self.stack.borrow();
+        if a.len() > 0 {
+            Some(a[a.len() - 1].clone())
+        } else{
+            None
+        }
     }
 }
 
@@ -33,28 +47,66 @@ impl<T: Clone> Clone for GlobalStack<T> {
 }
 
 fn main() {
-    let stack = GlobalStack::new(vec![1,2,3]);
-
-    println!("{stack:?}");
-
-    stack.push(4);
-
-    println!("{stack:?}");
-
-
-    let a = stack.pop();
-    println!("{stack:?}, {a}");
-
-    let a = stack.get_last();
-    println!("{stack:?}, {a}");
-
-    let stack2 = stack.clone();
-
-    println!("{stack:?}, {stack2:?}");
-
-    stack2.push(4);
-    stack.push(5);
-    println!("{stack:?}, {stack2:?}");
-
 }
 
+
+#[cfg(test)]
+mod test {
+    use std::alloc::GlobalAlloc;
+
+use super::*;
+
+    #[test]
+    fn test_new() {
+        let stack1: GlobalStack<i32> = GlobalStack::new(None);
+        let stack2 = GlobalStack { stack: Rc::new(RefCell::new(Vec::new())) };
+        let stack3 = GlobalStack { stack: Rc::new(RefCell::new(vec![1,2,3]))};
+        let stack4 = GlobalStack::new(Some(vec![1,2,3]));
+        assert_eq!(stack2, stack1);
+        assert_eq!(stack3, stack4);
+    }
+
+    #[test]
+    fn test_push() {
+        let stack1: GlobalStack<i32> = GlobalStack::new(None);
+        stack1.push(1);
+        stack1.push(2);
+        let stack2 = GlobalStack::new(Some(vec![1,2]));
+
+        assert_eq!(stack1, stack2);
+
+    }
+
+    #[test]
+    fn test_pop() {
+        let stack1: GlobalStack<i32> = GlobalStack::new(None);
+        assert_eq!(None, stack1.pop());
+        let stack2 = GlobalStack::new(Some(vec![1,2,3]));
+        assert_eq!(stack2.pop(), Some(3));
+        assert_eq!(stack2.pop(), Some(2));
+        assert_eq!(stack2.pop(), Some(1));
+    }
+
+    #[test]
+    fn test_clone(){
+        let stack1 = GlobalStack::new(Some(vec![1,2,3]));
+        let stack2 = stack1.clone();
+        assert_eq!(stack1.pop(), Some(3));
+        assert_eq!(stack2.pop(), Some(2));
+        assert_eq!(stack1.pop(), Some(1));
+        assert_eq!(stack2.pop(), None);
+    }
+
+    #[test]
+    fn test_get_last() {
+        let stack = GlobalStack::new(Some(vec![1,2,3]));
+        assert_eq!(stack.get_last(), Some(3));
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.get_last(), Some(2));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.get_last(), Some(1));
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.get_last(), None);
+        assert_eq!(stack.pop(), None);
+    }
+}
